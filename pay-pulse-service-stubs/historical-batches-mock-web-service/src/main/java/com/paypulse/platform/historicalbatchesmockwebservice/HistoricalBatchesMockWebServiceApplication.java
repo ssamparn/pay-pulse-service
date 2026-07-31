2f0c05175a808adfaf2dc0263325bedc1c984df0
Sashank <sashanka.samantray@rabobank.nl>
2026-07-31 23:52:05 +0200

    added new mock module

---
+++ pay-pulse-service-stubs/historical-batches-mock-web-service/src/main/java/com/paypulse/platform/historicalbatchesmockwebservice/HistoricalBatchesMockWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paypulse.platform.historicalbatchesmockwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalBatchesMockWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoricalBatchesMockWebServiceApplication.class, args);
+    }
+
+}
---
+++ pay-pulse-service-stubs/historical-batches-mock-web-service/src/test/java/com/paypulse/platform/historicalbatchesmockwebservice/HistoricalBatchesMockWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypulse.platform.historicalbatchesmockwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricalBatchesMockWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
